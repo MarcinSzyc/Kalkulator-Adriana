1fb16cb2a5b0f98fe12bc1aa291f07b1278693ca
MarcinSzyc <szyc.marcin@gmail.com>
2019-04-10 16:22:55 +0200

    poprawka w settings dot. static files

--- andzy/settings.py
+++ andzy/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ["/home/marcin/Desktop/projekty/Kalkulator-Adriana/andzy_calc/static"]
